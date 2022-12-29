6770e14978d4197705a719436f90e6ba717b55a3
Joe Shaju Perinchery <jperinch@lab1r6s8.42abudhabi.ae>
2022-12-29 16:17:16 +0400

    1

--- main.c
+++ main.c
@@ -2,10 +2,16 @@
 
 int	main(void)
 {
+	int i =0;
 	void	*mlx;
 	void	*mlx_win;
+		mlx = mlx_init();
+		mlx_win = mlx_new_window(mlx, 1500, 1000, "FDF");
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	mlx_loop(mlx);
+		mlx_new_image(mlx, 1920, 1080);
+		// mlx_pixel_put(mlx,mlx_win,-100,-10,0x0000f);
+		mlx_clear_window ( mlx, mlx_win );
+		mlx_pixel_put(mlx,mlx_win,-100,-10,0x0000f);
+		mlx_loop(mlx);
+
 }
